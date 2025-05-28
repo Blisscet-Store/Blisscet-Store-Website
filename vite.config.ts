c78913d758389e08844928ce315e406a9d015eb0
Blisscet-Store <blisscetstore@gmail.com>
2025-05-28 13:11:20 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-})
-
-build: {
-  chunkSizeWarningLimit: 1000, // increase limit if needed
-  rollupOptions: {
-    output: {
-      manualChunks: {
-        // Example: split vendor libraries into separate chunk
-        vendor: ['react', 'react-dom', 'other-large-library'],
-      }
-    }
-  }
-}
+    plugins: [react(), tailwindcss()],
+    build: {
+        chunkSizeWarningLimit: 1000, // increase limit if needed
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    // Example: split vendor libraries into separate chunk
+                    vendor: ["react", "react-dom", "other-large-library"],
+                },
+            },
+        },
+    },
+});
